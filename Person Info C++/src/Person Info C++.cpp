7703e4f5a6be3b1b53082ec8d2d0355aaad501d0
TEE <TEE@10.0.0.185>
2024-09-15 19:27:49 -0500

    Commit 1

---
+++ Person Info C++/src/Person Info C++.cpp
@@ -0,0 +1,66 @@
+// Fictional Person Info
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Variables
+	string first_name = "John";
+	string last_name = "Wick";
+	string address = "123 West Road";
+	string city = "New York City";
+	int zip_code = 10001;
+	// Display Info
+	cout << "First Name: " << first_name << endl;
+	cout << "Last Name: " << last_name << endl;
+	cout << "Address: " << address << endl;
+	cout << "City: " << city << endl;
+	cout << "Zip Code: " << zip_code << endl;
+	return 0;
+}
+
+/* Simple Program with a few Errors for Correction
+Please be sure to correct all outlined errors. */
+// Mod 1
+//#include<iostream>
+//#include<conio.h>
+//// Standard namespace declaration
+//using namespace std;
+//
+//// Main Function
+//int main() {
+//     //Standard Ouput Statement
+//     cout << "Welcome to this C++ Program" << endl;
+//
+//     cout << "I have corrected all errors for this program." << endl;
+//
+//     // Wait For Output Screen
+//
+//     //Main Function return Statement
+//     return 0;
+//}
+
+//#include<iostream>
+//#include<conio.h>
+//
+//// Standard namespace declaration
+//using namespace std;
+//
+//// Main Function
+//int main() {
+//
+//     double myMoney = 1000.50;  //this should be printed out
+//
+//     // Standard Ouput Statement
+//     cout << "Please be sure to correct all syntax errors in this program" << endl;
+//
+//     cout << "I have corrected all errors for this program." << endl;
+//
+//     cout << "The total amount of money available is = " << myMoney << endl;
+//
+//     // Wait For Output Screen
+//
+//     // Main Function return Statement
+//     return 0;
+//}
+
+
